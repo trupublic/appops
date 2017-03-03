fa7d6b24a185b2df9c638d7b71ec105a176070d1
Gautam <gautam@ainosoft.com>
2017-03-03 16:56:14 +0530

    Webserver updated with ability to run stand alone and needs testing

--- appops/appops-infra/appops-infra-web/src/main/java/org/appops/infra/InfraWebAppModule.java
+++ appops/appops-infra/appops-infra-web/src/main/java/org/appops/infra/InfraWebAppModule.java
@@ -1,12 +1,16 @@
 package org.appops.infra;
 
+import javax.inject.Singleton;
+
 import com.google.inject.servlet.ServletModule;
 
 public class InfraWebAppModule extends ServletModule  {
 	
 	 @Override
      protected void configureServlets() {
-       serve("/infraEntryPoint/*").with(WebAppEntryPoint.class);
+       serve("/infraEntryPoint").with(InfraEntryPoint.class);
+       bind(WebAppEntryPoint.class).in(Singleton.class);
+       bind(InfraEntryPoint.class).in(Singleton.class);
      }
 
 }
